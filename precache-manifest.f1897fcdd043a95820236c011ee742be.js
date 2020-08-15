c713f8c098dd401c5cf4b1ce5081dd703f75ac97
Daniel Vega <danielalexvega@gmail.com>
2020-08-14 22:54:01 -0500

    Updates

---
+++ precache-manifest.f1897fcdd043a95820236c011ee742be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0990cc231e3e670be055c0fe0fbcda02",
+    "url": "/reactDeployTest/index.html"
+  },
+  {
+    "revision": "22ce23bd957e876e27a8",
+    "url": "/reactDeployTest/static/css/main.956cefb0.chunk.css"
+  },
+  {
+    "revision": "d9d08bf7c50e1d7151c8",
+    "url": "/reactDeployTest/static/js/2.8d14717a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactDeployTest/static/js/2.8d14717a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ce23bd957e876e27a8",
+    "url": "/reactDeployTest/static/js/main.35cbe623.chunk.js"
+  },
+  {
+    "revision": "383f90c2acc394155465",
+    "url": "/reactDeployTest/static/js/runtime-main.edf2c2b2.js"
+  }
+]);
